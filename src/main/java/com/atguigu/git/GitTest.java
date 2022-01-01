6dae82fcf8a4d36f944de24d138483cb981e013a
sxdtjsblxb <1978858595@qq.com>
2022-01-01 11:44:46 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author LXB
+ * @create 2022-01-01 11:34
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
